9d8ff0b5d0480f1a882c5eed95472965c811d67f
Larry <larryotieno51@gmail.com>
2025-06-24 09:50:42 +0300

    Created the server side file structure successfully

---
+++ server/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from flask import request, session
+from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
+
+from config import app, db, api
+from models import User
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
---
+++ server/models.py
@@ -0,0 +1,8 @@
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy_serializer import SerializerMixin
+
+from config import db, bcrypt
+
+class User(db.Model, SerializerMixin):
+    pass
